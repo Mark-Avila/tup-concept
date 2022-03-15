d61a130a45620f508f2ac9dcaa734bf4406fc8a4
markavila <markavila.dev@gmail.com>
2022-03-15 21:55:56 +0800

    footer

--- public/index.js
+++ public/index.js
@@ -19,3 +19,26 @@ function fetchCarouselData() {
 
   console.log(carouselJson);
 }
+
+function openFooterLinks(link) {
+  let element;
+
+  if (link === "popular") {
+    element = document.querySelector("#footer-popular");
+  } else if (link === "academics") {
+    element = document.querySelector("#footer-academics");
+  } else if (link === "govph") {
+    element = document.querySelector("#footer-govph");
+  } else if (link === "govlinks") {
+    element = document.querySelector("#footer-govlinks");
+  }
+
+  let isVisible = window.getComputedStyle(element, null).display;
+
+  if (isVisible === "flex") {
+    element.style.display = "none";
+  }
+  if (isVisible === "none") {
+    element.style.display = "flex";
+  }
+}
